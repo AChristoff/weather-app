13ee27cf0305d8779c655a94204f18d910f49628
AChristoff <aleksov101@gmail.com>
2022-03-27 22:46:02 +0300

    deploy on github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/dashboard/Dashboard';
 
 function App() {
   return (
     <div id="app">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-        </Routes>
-      </Router>
+      <Dashboard />
+      {/* 
+        <Router> 
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+          </Routes>
+        </Router> 
+      */}
     </div>
   );
 }
